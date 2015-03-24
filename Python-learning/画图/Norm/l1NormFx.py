b879d1fbd004d7e9c7c6a6b9d79df676364007eb
muzichao <568191222@qq.com>
2015-03-24 18:56:25 +0800

    matplotlib norm

---
+++ Python-learning/画图/Norm/l1NormFx.py
@@ -0,0 +1,69 @@
+from matplotlib.patches import Ellipse, Circle
+import matplotlib.pyplot as plt
+
+###########################################################
+# define a function to draw a quadrangle
+def drawQuadrangle(p1, p2, p3, p4, cl = 'b', lw = 1, ls = '-'):
+    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], color = cl, linewidth = lw, linestyle = ls)
+    plt.plot([p2[0], p3[0]], [p2[1], p3[1]], color = cl, linewidth = lw, linestyle = ls)
+    plt.plot([p3[0], p4[0]], [p3[1], p4[1]], color = cl, linewidth = lw, linestyle = ls)
+    plt.plot([p4[0], p1[0]], [p4[1], p1[1]], color = cl, linewidth = lw, linestyle = ls)
+
+
+###########################################################
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+###########################################################
+# plot quadrangle
+squareSize = 2;
+p1 = [-squareSize, 0]
+p2 = [0, squareSize]
+p3 = [squareSize, 0]
+p4 = [0, -squareSize]
+
+drawQuadrangle(p1, p2, p3, p4, cl = 'b', lw = 2, ls = '-')
+
+
+###########################################################
+# plot circle
+x, y = 1.5*squareSize, 2*squareSize # center
+ell = Ellipse(xy = (x, y), width = squareSize, height = squareSize, angle = 30.0, facecolor= 'yellow', alpha = 0.5)
+ax.add_patch(ell)
+ax.plot(x, y, 'ro') # plot center
+
+ell = Ellipse(xy = (x, y), width = 1.5 * squareSize, height = 1.5 * squareSize, angle = 30.0, facecolor= 'green', alpha=0.3)
+ax.add_patch(ell)
+ax.plot(x, y, 'ro') # plot center
+
+ell = Ellipse(xy = (x, y), width = 2 * squareSize, height = 2 * squareSize, angle = 30.0, facecolor= 'green', alpha=0.3)
+ax.add_patch(ell)
+ax.plot(x, y, 'ro') # plot center
+
+###########################################################
+# set axis
+axisSize = 8;
+plt.axis('equal')
+plt.xlim(-axisSize, axisSize)
+
+plt.axis('equal')
+plt.ylim(-axisSize, axisSize)
+
+# set x, y free in side
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+# set x in center
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+
+#set y in center
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+
+# set ticklabel
+ax.set_yticklabels([''])
+ax.set_xticklabels([''])
+
+###########################################################
+plt.show()
